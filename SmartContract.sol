6b09aec0ed254fcbad9e2eeb2d13c16792c6d9fb
Viraj Patva <68849696+19IT117@users.noreply.github.com>
2022-03-30 11:07:19 +0530

    Added Case only authorized person can upload Hash
    Also updated readability of Code

--- SmartContract.sol
+++ SmartContract.sol
@@ -1,32 +1,40 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.7.6;
 
 contract VerifyCertificate {
-    //---mapping is an associative array, similar to key/value pairs
-    // key is bytes32 and value is boolean---
+    
+    //---Issuer is an authority who can store hash of certificate in the blockchain---
+
+    address public issuer;
     mapping (string => bool) private proofs;
-
-    //---stores the hash of the lyrics in the mapping---
-    function storeProof(string proof) private {
-    //require(hash != null);
-  //  require(proof!="" );// it should not null && uploaded by manager.
-    proofs[proof] = true;
+   
+   //---Storing the address of Issuer in Smart Contract---//
+    constructor(){
+        issuer = msg.sender;
     }
-
-    //---calculate and store the hash (proof) for a song's lyrics---
-    function upload_Hash(string file_hash) public {
+    
+    //--Only Issuer can upload hash in blockchain---//
+    function upload_Hash(string memory file_hash) public {
+        require(msg.sender == issuer,"You must be a valid issuer to issue certificate");
         storeProof(file_hash);
     }
 
-    //---check if a lyrics has previously been saved---
-    function verifyCertificate(string file_hash) public view returns (bool) {
-        return hasProof(file_hash);
+    //-- Private function which will store hash in blockchain , it also checks that hash should not be blank--//
+    function storeProof(string memory proof) private {
+        require(bytes(proof).length > 0,"You cannot upload blank hash");
+        proofs[proof] = true;
     }
 
-    //---returns true if proof is found---
-    function hasProof(string proof) private view
-    returns(bool) {
+  
+    //--- While verifying the certificate one can upload the hash of the Certificate to the function and check if its valid or not---//
+    function verifyCertificate(string  memory file_hash) public view returns (bool) {
+        require(bytes(file_hash).length > 0,"While verifying hash you cannot keep it blank");
+        return hasProof(file_hash);
+    }
+    
+    //--- It will return true if the given certificate is valid ---//
+    function hasProof(string memory proof) private view returns(bool) {
         return proofs[proof];
     }
 }
-
 
