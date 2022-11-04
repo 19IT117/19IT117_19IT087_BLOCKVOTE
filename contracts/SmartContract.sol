6b06395d9061a3fcd3ef692c9c45e556241c12cf
vkpatva <19it117@charusat.edu.in>
2022-11-04 20:49:19 +0530

    smart contract updated

--- contracts/SmartContract.sol
+++ contracts/SmartContract.sol
@@ -1,39 +1,37 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.7.6;
+pragma solidity ^0.8.0;
 
-contract VerifyCertificate {
+contract Verifile {
     
-    //---Issuer is an authority who can store hash of certificate in the blockchain---
-
     address public issuer;
-    mapping (string => bool) private proofs;
-   
-   //---Storing the address of Issuer in Smart Contract---//
+    mapping (bytes => bool) private proofs;
     constructor(){
         issuer = msg.sender;
     }
-    
-    //--Only Issuer can upload hash in blockchain---//
-    function upload_Hash(string memory file_hash) public {
-        require(msg.sender == issuer,"You must be a valid issuer to issue certificate");
+    function issue(bytes memory file_hash) public {
+        require(msg.sender == issuer,"Invalid Issuer");
         storeProof(file_hash);
     }
+    function revoke(bytes memory file_hash) public {
+        require(msg.sender == issuer , "Invalid Revoker");
+        revokeProof(file_hash);
+    }
 
-    //-- Private function which will store hash in blockchain , it also checks that hash should not be blank--//
-    function storeProof(string memory proof) private {
+    function storeProof(bytes memory proof) private {
+        require(bytes(proof).length > 0,"You cannot upload blank hash");
+        proofs[proof] = true;
+    }
+    function revokeProof(bytes memory proof) private {
         require(bytes(proof).length > 0,"You cannot upload blank hash");
         proofs[proof] = true;
     }
 
-  
-    //--- While verifying the certificate one can upload the hash of the Certificate to the function and check if its valid or not---//
-    function verifyCertificate(string  memory file_hash) public view returns (bool) {
+    function verifyCertificate(bytes memory file_hash) public view returns (bool) {
         require(bytes(file_hash).length > 0,"While verifying hash you cannot keep it blank");
         return hasProof(file_hash);
     }
-    
-    //--- It will return true if the given certificate is valid ---//
-    function hasProof(string memory proof) private view returns(bool) {
+
+    function hasProof(bytes memory proof) private view returns(bool) {
         return proofs[proof];
     }
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Verifile = artifacts.require("Verifile");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Verifile);
 };
